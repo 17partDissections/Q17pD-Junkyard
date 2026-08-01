869fb2fbbea52be2a1c3f1776c7ca6145abeef66
Andrew <159803151+17partDissections@users.noreply.github.com>
2026-08-01 18:56:00 +0300

    New hosting update

--- bottom.js
+++ bottom.js
@@ -2,25 +2,25 @@ document.addEventListener("DOMContentLoaded", async () => {
   const bottom = `
     <div id="bottomer">
       <div class="badges8831">
-			<a href="https://notepad-plus-plus.org"target="_blank"><img src="/Q17pD-Junkyard/assets/gifs/badges/notepadpp.gif" width="88" height="31"></a>
-			<a href="https://t.me/nyunuaa"target="_blank"><img src="/Q17pD-Junkyard/assets/gifs/badges/konata.gif" width="88" height="31"></a>
-			<img src="/Q17pD-Junkyard/assets/gifs/badges/trans.gif" width="88" height="31">
-			<img src="/Q17pD-Junkyard/assets/gifs/badges/underwear.gif" width="88" height="31">
-			<a href="https://www.crytek.com/games/farcry"target="_blank"> <img src="/Q17pD-Junkyard/assets/gifs/badges/farcry.gif" width="88" height="31"></a>
-			<img src="/Q17pD-Junkyard/assets/gifs/badges/i-hate-microsoft.gif" width="88" height="31">
+			<a href="https://notepad-plus-plus.org"target="_blank"><img src="/assets/gifs/badges/notepadpp.gif" width="88" height="31"></a>
+			<a href="https://t.me/nyunuaa"target="_blank"><img src="/assets/gifs/badges/konata.gif" width="88" height="31"></a>
+			<img src="/assets/gifs/badges/trans.gif" width="88" height="31">
+			<img src="/assets/gifs/badges/underwear.gif" width="88" height="31">
+			<a href="https://www.crytek.com/games/farcry"target="_blank"> <img src="/assets/gifs/badges/farcry.gif" width="88" height="31"></a>
+			<img src="/assets/gifs/badges/i-hate-microsoft.gif" width="88" height="31">
       </div>
 	  <div class="badges8831">
-			<a href="https://www.mirc.com"target="_blank"><img src="/Q17pD-Junkyard/assets/gifs/badges/mirc.gif" width="88" height="31"></a>
-			<a href="https://scp-wiki.wikidot.com"target="_blank"><img src="/Q17pD-Junkyard/assets/gifs/badges/scp.gif" width="88" height="31"></a>
-			<img src="/Q17pD-Junkyard/assets/gifs/badges/bluepantsu.gif" width="88" height="31">
-			<a href="https://www.blender.org"target="_blank"><img src="/Q17pD-Junkyard/assets/gifs/badges/blender.gif" width="88" height="31"></a>
-			<a href="https://www.win-rar.com"target="_blank"> <img src="/Q17pD-Junkyard/assets/gifs/badges/winrar.gif" width="88" height="31"></a>
-			<img src="/Q17pD-Junkyard/assets/gifs/badges/pn.gif" width="88" height="31">
+			<a href="https://www.mirc.com"target="_blank"><img src="/assets/gifs/badges/mirc.gif" width="88" height="31"></a>
+			<a href="https://scp-wiki.wikidot.com"target="_blank"><img src="/assets/gifs/badges/scp.gif" width="88" height="31"></a>
+			<img src="/assets/gifs/badges/bluepantsu.gif" width="88" height="31">
+			<a href="https://www.blender.org"target="_blank"><img src="/assets/gifs/badges/blender.gif" width="88" height="31"></a>
+			<a href="https://www.win-rar.com"target="_blank"> <img src="/assets/gifs/badges/winrar.gif" width="88" height="31"></a>
+			<img src="/assets/gifs/badges/pn.gif" width="88" height="31">
 			
       </div>
       <div id="bottom-notes">
 		<br>
-        <a href="/Q17pD-Junkyard/license.html" style="color: orange;">© 2026 Andrew Q17pD</a><br>
+        <a href="/license.html" style="color: orange;">© 2026 Andrew Q17pD</a><br>
 		Website last update: 23.07.2026
       </div>
     </div>
